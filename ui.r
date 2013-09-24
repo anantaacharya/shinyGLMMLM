a57397fdebd23f6ca1af8c02e1f15f67f950dd78
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-24 17:11:15 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -7,32 +7,32 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
  
     wellPanel(
-      checkboxGroupInput("GLMMLM", "GLMMLM:",
+      selectInput("GLMMLM", "GLMMLM:",
                    c("GLM" = "GLM",
                      "MLM" = "MLM",
                      "K-EMMA" = "K-EMMA",
-                      "K-SSR"="K-SSR"), selected="K-EMMA")
+                      "K-SSR"="K-SSR"), selected="K-EMMA", multiple=TRUE)
  
       
     ),
  
    wellPanel(
-      checkboxGroupInput("model", "model:",
+      selectInput("model", "model:",
                    c("NAIVE" = "NAIVE",
                      "PC" = "PC",
-                     "Q" = "Q"), selected="Q")
+                     "Q" = "Q"), selected="Q", multiple=TRUE)
  
       
     ),
 
   wellPanel(
-    checkboxGroupInput("df", "data:",
+    selectInput("df", "data:",
                    c("10k20" = "10k20",
                      "10k50" = "10k50",
                      "UNEAK3" = "UNEAK3",
                       "UNEAK4"="UNEAK4",
                       "UNEAK8" = "UNEAK8",
-                      "UNEAK9"="UNEAK9"),selected="10k20")
+                      "UNEAK9"="UNEAK9"),selected="10k20", multiple=TRUE)
  
      
     )
