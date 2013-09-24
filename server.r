227cbd9b0c08ccdaf3138b2791ad54760b421a1f
ananta acharya <ananta.acharya@gmail.com>
2013-09-24 16:47:17 -0500

    gist to github

---
+++ server.r
@@ -0,0 +1,28 @@
+library(shiny)
+library(ggplot2)
+
+
+masterglmmlm.test <- read.csv("masterglmmlm.q.csv")
+
+ 
+shinyServer(function(input, output) {
+ 
+
+ 
+  output$main_plot <- renderPlot(function(){
+    #get subset of data based on selection
+   gl1<-input$GLMMLM
+   df1<-input$df
+   model1<-input$model1
+gl1<-c("MLM","K-EMMA")
+df1<-c("10k20","UNEAK9")
+model1<-c("PC","Q")
+ plot.temp<-masterglmmlm.test
+    #plot.temp<-masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,]
+    p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=plot.temp)+geom_point(aes(shape=df))+facet_wrap(~Locus)
+   
+    print(p)
+  })
+ 
+ 
+})
---
+++ ui.r
@@ -0,0 +1,51 @@
+library(shiny)
+ 
+shinyUI(pageWithSidebar(
+ 
+  headerPanel("GLM MLM p values"),
+ 
+  sidebarPanel(
+ 
+    wellPanel(
+      checkboxGroupInput("GLMMLM", "GLMMLM:",
+                   c("GLM" = "GLM",
+                     "MLM" = "MLM",
+                     "K-EMMA" = "K-EMMA",
+                      "K-SSR"="K-SSR"), selected="K-EMMA")
+ 
+      
+    ),
+ 
+   wellPanel(
+      checkboxGroupInput("model", "model:",
+                   c("NAIVE" = "NAIVE",
+                     "PC" = "PC",
+                     "Q" = "Q"), selected="Q")
+ 
+      
+    ),
+
+  wellPanel(
+    checkboxGroupInput("df", "data:",
+                   c("10k20" = "10k20",
+                     "10k50" = "10k50",
+                     "UNEAK3" = "UNEAK3",
+                      "UNEAK4"="UNEAK4",
+                      "UNEAK8" = "UNEAK8",
+                      "UNEAK9"="UNEAK9"),selected="10k20")
+ 
+     
+    )
+ 
+    
+ 
+    
+  ),
+ 
+  mainPanel(
+    plotOutput(outputId = "main_plot")
+ 
+  
+ 
+  )
+))
