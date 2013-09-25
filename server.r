d2d0952ebc80fa5a12a0171f690d394ea2ecfa8d
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-25 15:20:25 -0500

    Update server.r

--- server.r
+++ server.r
@@ -16,12 +16,12 @@ shinyServer(function(input, output) {
     gl1<-input$GLMMLM
    df1<-input$df
    model1<-input$model1
-   tr1<-c("ALL")
-   gl1<-c("MLM","K-EMMA")
-df1<-c("10k20","UNEAK9")
-model1<-c("PC","Q")
+   #tr1<-c("ALL")
+   #gl1<-c("MLM","K-EMMA")
+#df1<-c("10k20","UNEAK9")
+#model1<-c("PC","Q")
  
-    masterglmmlm.test[masterglmmlm.test$Trait%in% tr1 & masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
+    subset(masterglmmlm.test,Trait %in% tr1 & GLMMLM %in% gl1 & model %in% model1 & df %in% df1,drop=FALSE)
     
   })
    tr1<-c("ALL")
@@ -29,13 +29,18 @@ model1<-c("PC","Q")
 df1<-c("10k20","UNEAK9")
 model1<-c("PC","Q")
  
-    m2<-masterglmmlm.test[masterglmmlm.test$Trait%in% tr1 & masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
+    m2<-subset(masterglmmlm.test,Trait %in% tr1 & GLMMLM %in% gl1 & model %in% model1 & df %in% df1,drop=FALSE)
     
 
     
-    p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=masterglmmlm.test)+geom_point(aes(shape=df))+facet_wrap(~Locus)
+    p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=m2)+geom_point(aes(shape=df))+facet_wrap(~Locus)
    
     print(p)
+    
+     q<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=plot.temp())+geom_point(aes(shape=df))+facet_wrap(~Locus)
+   
+    print(q)
+    
   })
   
   
