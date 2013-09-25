6bcf741939e2df9882f660325dc4f51085aa2837
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-24 22:54:53 -0500

    Update server.r

--- server.r
+++ server.r
@@ -15,9 +15,9 @@ shinyServer(function(input, output) {
     gl1<-input$GLMMLM
    df1<-input$df
    model1<-input$model1
-   gl1<-c("MLM","K-EMMA")
-df1<-c("10k20","UNEAK9")
-model1<-c("PC","Q")
+   #gl1<-c("MLM","K-EMMA")
+#df1<-c("10k20","UNEAK9")
+#model1<-c("PC","Q")
  
     masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
     
