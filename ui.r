ed4120a33ffc802b3daae124818f52911e001e13
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-24 22:46:43 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -8,31 +8,31 @@ shinyUI(pageWithSidebar(
  
     
       selectInput("GLMMLM", "GLMMLM:",
-                   c("GLM" ,
-                     "MLM" ,
-                     "K-EMMA" ,
-                      "K-SSR"), selected="K-EMMA", multiple=TRUE)
+                   list("GLM"="GLM" ,
+                     "MLM"="MLM" ,
+                     "K-EMMA" ="K-EMMA",
+                      "K-SSR"="K-SSR"), selected="K-EMMA", multiple=TRUE)
  
       
     ,
  
    
       selectInput("model", "model:",
-                   c("NAIVE" ,
-                     "PC" ,
-                     "Q" ), selected="Q", multiple=TRUE)
+                   list("NAIVE" ="NAIVE",
+                     "PC" ="PC",
+                     "Q" ="Q"), selected="Q", multiple=TRUE)
  
       
     ,
 
  
     selectInput("df", "data:",
-                   c("10k20" ,
-                     "10k50" ,
-                     "UNEAK3" ,
-                      "UNEAK4",
-                      "UNEAK8" ,
-                      "UNEAK9"),selected="10k20", multiple=TRUE)
+                   list("10k20" ="10k20",
+                     "10k50" ="10k50",
+                     "UNEAK3" ="UNEAK3",
+                      "UNEAK4"="UNEAK4",
+                      "UNEAK8" ="UNEAK8",
+                      "UNEAK9"="UNEAK9"),selected="10k20", multiple=TRUE)
  
      
     
