37ff3262e832a39556886b64d295350a8c3303c5
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-25 12:28:37 -0500

    changing selectinput to checkboxinput

--- ui.r
+++ ui.r
@@ -6,40 +6,39 @@ shinyUI(pageWithSidebar(
  
   sidebarPanel(
  
+    wellPanel(
     
-      selectInput("GLMMLM", "GLMMLM:",
-                   list("GLM"="GLM" ,
-                     "MLM"="MLM" ,
-                     "K-EMMA" ="K-EMMA",
-                      "K-SSR"="K-SSR"), selected="K-EMMA", multiple=TRUE)
+    checkboxInput("GLM",    label = "GLM"),
+    checkboxInput("MLM",    label = "MLM"),
+    checkboxInput("K-EMMA",    label = "K-EMMA"),
+    checkboxInput("K-SSR",    label = "K-SSR")
+      
  
       
-    ,
+    ),
  
+   wellPanel(
    
-      selectInput("model", "model:",
-                   list("NAIVE" ="NAIVE",
-                     "PC" ="PC",
-                     "Q" ="Q"), selected="Q", multiple=TRUE)
- 
+   checkboxInput("NAIVE",    label = "NAIVE"),
+    checkboxInput("PC",    label = "PC"),
+    checkboxInput("Q",    label = "Q")
+    
       
-    ,
+    ),
+
+ wellPanel(
+ 
+ 
+ checkboxInput("10k20",    label = "10k20"),
+    checkboxInput("10k50",    label = "10k50"),
+    checkboxInput("UNEAK3",    label = "UNEAK3"),
+    checkboxInput("UNEAK4",    label = "UNEAK4"),
+    checkboxInput("UNEAK8",    label = "UNEAK8"),
+    checkboxInput("UNEAK9",    label = "UNEAK9")
+    
 
  
-    selectInput("df", "data:",
-                   list("10k20" ="10k20",
-                     "10k50" ="10k50",
-                     "UNEAK3" ="UNEAK3",
-                      "UNEAK4"="UNEAK4",
-                      "UNEAK8" ="UNEAK8",
-                      "UNEAK9"="UNEAK9"),selected="10k20", multiple=TRUE)
- 
-     
-    
- 
-    
- 
-    
+   ) 
   ),
  
   mainPanel(
