30cbad9ab2164e46fe66ba4eba33baf56efbf7dd
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-25 13:15:29 -0500

    Update server.r

--- server.r
+++ server.r
@@ -2,65 +2,35 @@ library(shiny)
 library(ggplot2)
 
 
-masterglmmlm.test <- read.csv("masterglmmlm.q.csv", stringsAsFactors=FALSE)
+masterglmmlm.test <- read.csv("masterglmmlm.q.csv", header=T, stringsAsFactors=FALSE)
+#make everythong character
+masterglmmlm.test[,1]<-as.character(masterglmmlm.test[,1])
+masterglmmlm.test[,2]<-as.character(masterglmmlm.test[,1])
+masterglmmlm.test[,12]<-as.character(masterglmmlm.test[,12])
+masterglmmlm.test[,13]<-as.character(masterglmmlm.test[,13])
+masterglmmlm.test[,14]<-as.character(masterglmmlm.test[,14])
+masterglmmlm.test[,15]<-as.character(masterglmmlm.test[,15])
 
- 
 shinyServer(function(input, output) {
  
- output$textout<-renderText({
- gl1=vector()
-    if (input$GLM) {
-      gl1<-c(gl1,"GLM")
-    }
-    if (input$MLM) {
-      gl1<-c(gl1,"MLM")
-    }
-    if (input$K-SSR) {
-      gl1<-c(gl1,"K-SSR")
-    }
-    if (input$K-EMMA) {
-      gl1<-c(gl1,"K-EMMA")
-    }
-   model1<-vector()
-   if (input$NAIVE) {
-      model1<-c(model1,"NAIVE")
-    }
-    if (input$PC) {
-      model1<-c(model1,"PC")
-    }
-    if (input$Q) {
-      model1<-c(model1,"Q")
-    }
-   
-   
-   df1<-vector()
-   if (input$s10k20) {
-      df1<-c(df1,"10k20")
-    }
-    if (input$s10k50) {
-      df1<-c(df1,"10k50")
-    }
-    if (input$UNEAK3) {
-      df1<-c(df1,"UNEAK3")
-    }
-    if (input$UNEAK4) {
-      df1<-c(df1,"UNEAK4")
-    }
-    if (input$UNEAK8) {
-      df1<-c(df1,"UNEAK8")
-    }
-    if (input$UNEAK9) {
-      df1<-c(df1,"UNEAK9")
-    }
-   
-   print(paste(df1, model1, gl1))
- })
  
    
  
   output$main_plot <- renderPlot({
     #get subset of data based on selection
     plot.temp <- reactive({
+    
+    tr1=vector(mode="character")
+    if (input$All) {
+      tr1<-c(trl1,"yBLUP")
+    }
+     if (input$ATH) {
+      tr1<-c(trl1,"yBLUPATH")
+    }
+     if (input$ARD) {
+      tr1<-c(trl1,"yBLUP.ARD")
+    }
+    
     #if else for kinships
     gl1=vector(mode="character")
     if (input$GLM) {
@@ -111,8 +81,8 @@ shinyServer(function(input, output) {
    #gl1<-c("MLM","K-EMMA")
 #df1<-c("10k20","UNEAK9")
 #model1<-c("PC","Q")
- 
-    masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
+ #tr1<-c("yBLUP")
+    masterglmmlm.test[masterglmmlm.test$Trait %in% tr1 & masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
     
   })
    
