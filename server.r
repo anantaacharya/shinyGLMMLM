f53200bb836339973343aa51fb381a080f7e3011
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-24 21:51:52 -0500

    Update server.r

--- server.r
+++ server.r
@@ -2,7 +2,7 @@ library(shiny)
 library(ggplot2)
 
 
-masterglmmlm.test <- read.csv("masterglmmlm.q.csv")
+masterglmmlm.test <- read.csv("masterglmmlm.q.csv", stringsAsFactors=F)
 
  
 shinyServer(function(input, output) {
@@ -11,13 +11,14 @@ shinyServer(function(input, output) {
  
   output$main_plot <- renderPlot({
     #get subset of data based on selection
+    #plot.temp<-reative({
    gl1<-input$GLMMLM
    df1<-input$df
    model1<-input$model1
 #gl1<-c("MLM","K-EMMA")
 #df1<-c("10k20","UNEAK9")
 #model1<-c("PC","Q")
- plot.temp<-masterglmmlm.test[1,]
+ #plot.temp<-masterglmmlm.test[1,]
     plot.temp<-masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
     
     p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=plot.temp)+geom_point(aes(shape=df))+facet_wrap(~Locus)
