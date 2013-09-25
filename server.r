61b09840373f267107c1e3d20195951a3ad74262
Ananta Raj Acharya <ananta.acharya@gmail.com>
2013-09-24 22:40:54 -0500

    Update server.r

--- server.r
+++ server.r
@@ -11,17 +11,20 @@ shinyServer(function(input, output) {
  
   output$main_plot <- renderPlot({
     #get subset of data based on selection
-    #plot.temp<-reative({
-   gl1<-input$GLMMLM
+    plot.temp <- reactive({
+    gl1<-input$GLMMLM
    df1<-input$df
    model1<-input$model1
+    masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
+    
+  })
+   
 #gl1<-c("MLM","K-EMMA")
 #df1<-c("10k20","UNEAK9")
 #model1<-c("PC","Q")
  #plot.temp<-masterglmmlm.test[1,]
-    plot.temp<-masterglmmlm.test[masterglmmlm.test$GLMMLM %in% gl1 & masterglmmlm.test$model %in% model1 & masterglmmlm.test$df %in% df1,,drop=FALSE]
     
-    p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=plot.temp)+geom_point(aes(shape=df))+facet_wrap(~Locus)
+    p<-ggplot(aes(pos, -log10(p), colour=interaction(GLMMLM, model)), data=plot.temp())+geom_point(aes(shape=df))+facet_wrap(~Locus)
    
     print(p)
   })
